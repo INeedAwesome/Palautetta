4dd651975c56f54443cf5c612b2f16a6bd02dcc4
Mattis Kjellerås <mattis@gr8.site>
2022-11-07 16:02:59 +0100

    Hello World!

--- src/main/java/site/gr8/mattis/palautetta/client/PalautettaClient.java
+++ src/main/java/site/gr8/mattis/palautetta/client/PalautettaClient.java
@@ -3,10 +3,14 @@ package site.gr8.mattis.palautetta.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Environment(EnvType.CLIENT)
 public class PalautettaClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		Logger logger = LoggerFactory.getLogger(PalautettaClient.class);
+		logger.info("Hello World!");
 	}
 }
