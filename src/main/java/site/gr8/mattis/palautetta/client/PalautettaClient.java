a8a217829d395e80a8d5d2f936ec98b3e50c6f82
Mattis Kjellerås <mattis@gr8.site>
2022-11-07 15:24:54 +0100

    First Commit

---
+++ src/main/java/site/gr8/mattis/palautetta/client/PalautettaClient.java
@@ -0,0 +1,13 @@
+package site.gr8.mattis.palautetta.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PalautettaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
